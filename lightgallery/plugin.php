0618cfd2f4e0e832c15af6ce1f3241f5c53b7d60
Karsten Silkenbäumer <kassi@users.noreply.github.com>
2018-06-12 11:30:55 +0200

    Add hooks to include lightgallery into album pages

--- lightgallery/plugin.php
+++ lightgallery/plugin.php
@@ -4,5 +4,50 @@ class KokenLightgalleryPlugin extends KokenPlugin {
 
 	function __construct() {
 		$this->require_setup = false;
+		$this->register_hook('before_closing_head', 'renderJsCss');
+		$this->register_hook('before_closing_body', 'renderBody');
+	}
+
+	// Load resources and render scripts to the page
+	function renderJsCss() {
+		$path = $_SERVER['REQUEST_URI'];
+		if (! preg_match('/\?\/albums\/.+/', $path)) {
+			return;
+		}
+
+		$js_path = $this->get_path() . '/js/plugin.js';
+
+		echo <<<HTML
+	<koken:if true="settings.use_lightgallery">
+		<script src="$js_path"></script>
+	</koken:if>
+HTML;
+	}
+
+	function renderBody() {
+		$path = $_SERVER['REQUEST_URI'];
+		if (! preg_match('/\?\/albums\/.+/', $path)) {
+			return;
+		}
+
+		echo <<<HTML
+	<koken:if true="settings.use_lightgallery">
+		<koken:load>
+		<koken:loop>
+		<script>
+		  LightGallery.addImage({
+		    index: {{index}},
+		    src: "{{content.presets.huge.url}}",
+		    mobileSrc: "{{content.presets.large.url}}",
+		    thumb: "{{content.presets.small.url}}",
+		    subHtml: '<h2>{{content.title || content.filename}}</h2><p>{{content.caption}}</p>',
+		    <koken:exif>exif: {{exif.to_json}},</koken:exif>
+		    <koken:geolocation>geolocation: {{geolocation.to_json}},</koken:geolocation>
+		  });
+		</script>
+		</koken:loop>
+		</koken:load>
+	</koken:if>
+HTML;
 	}
 }
