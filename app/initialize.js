38befb288e369cd2777227c123dd60b5461c24a5
Karsten Silkenbäumer <kassi@users.noreply.github.com>
2018-06-12 11:28:44 +0200

    Provide lightgallery functionality to app

--- app/initialize.js
+++ app/initialize.js
@@ -3,6 +3,54 @@ require('lg-fullscreen');
 require('lg-thumbnail');
 require('lg-zoom');
 
+var LightGallery = (function () {
+  var images = [];
+
+  var addImage = function (data) {
+    images[data['index']] = data;
+  };
+
+  var open = function (elem, index) {
+    $(elem).lightGallery({
+      hideBarsDelay: 3000,
+      index: index,
+      // controls: false,
+      // appendSubHtmlTo: '.lg-item',
+      dynamic: true,
+      dynamicEl: images,
+      mode: 'lg-fade',
+      download: true,
+      // share: false,
+      // hash: false,
+      // pager: false,
+      // selector: 'a',
+      thumbnail: true,
+      toggleThumb: true,
+      showThumbByDefault: false,
+      zoom: true,
+      actualSize: false
+    });
+  };
+
+  return {
+    addImage: addImage,
+    open: open
+  };
+})();
+window.LightGallery = LightGallery;
+
 document.addEventListener('DOMContentLoaded', function () {
-  // initialize code
+  function clickEvent (event) {
+    event.preventDefault();
+    var index = parseInt($(event.target).attr('data-index'), 10);
+    LightGallery.open(event.target, index);
+  }
+  $(function () {
+    $('#grid a').on('click', clickEvent);
+
+    $(window).on('k-infinite-loaded', function (event, html) {
+      $('#grid a').off('click', clickEvent);
+      $('#grid a').on('click', clickEvent);
+    });
+  });
 });
